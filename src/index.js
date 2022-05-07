437ebcd85bfb0a6234e07956b404888ddc2f1eb1
danton03 <37675973+danton03@users.noreply.github.com>
2022-05-07 17:31:49 -0300

    :tada: Projeto Finalizado

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom";
 import Navbar from "./components/Navbar";
 import Corpo from "./components/Corpo";
+import FundoMobile from "./components/FundoMobile";
 
 function App() {
   return(
     <>
       <Navbar />
       <Corpo />
+      <FundoMobile />
     </>
   );  
 }
